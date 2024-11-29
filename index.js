30f0b93db4d6163c3bf7c170ac661eed96c34d7a
ranamiah221 <ranarasul21@gmail.com>
2024-11-29 10:59:44 +0600

    added dynamic booking delete

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/booking/:id', async(req, res)=>{
+      const id= req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result);
+    })
+
 
    app.post('/booking', async(req, res)=>{
     const booking = req.body;
